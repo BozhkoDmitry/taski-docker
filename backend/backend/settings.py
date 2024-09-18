026ed5fa3e1c553bd1aa6cef5867276c15816ba4
Dmitry Bozhko <dmitry.bozhko.inbox@yandex.ru>
2024-09-18 22:24:25 +0700

    add another host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['123.123.123.123', 'localhost', '51.250.100.96', 'practicumtaski.ddnsking.com']
+ALLOWED_HOSTS = ['127.0.0.1', '123.123.123.123', 'localhost', '51.250.100.96', 'practicumtaski.ddnsking.com']
 
 
 # Application definition
